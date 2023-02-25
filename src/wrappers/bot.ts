84402b9dd01617f2a6c11593cae9ea2b27ab75bc
Nikita Monokov <nmonokov@gmail.com>
2023-02-25 08:35:49 +0200

    Remove encoding

--- src/wrappers/bot.ts
+++ src/wrappers/bot.ts
@@ -23,7 +23,7 @@ export class TelegramBot {
     const url = `${this._url}/sendPhoto`;
     const body = {
       chat_id: chatId.toString(),
-      photo: encodeURI(photoUrl),
+      photo: photoUrl,
       reply_markup: [[{
         inline_keyboard: {
           text: 'Reimagine',
